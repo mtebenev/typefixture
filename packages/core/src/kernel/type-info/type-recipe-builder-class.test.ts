e70cb2802acace50cdbc6da0cf8364343b6bddaa
Maxim Tebenev <mtebenev@gmail.com>
2019-07-01 06:38:59 +0300

    Class builder supports explicit ctors

--- packages/core/src/kernel/type-info/type-recipe-builder-class.test.ts
+++ packages/core/src/kernel/type-info/type-recipe-builder-class.test.ts
@@ -4,38 +4,70 @@ import {TypeRecipeBuilderClass} from './type-recipe-builder-class';
 import {ITypeRecipeContext} from './itype-recipe-context';
 import {ITypeRecipeRequest, TypeRecipeRequestKind} from './itype-recipe-request';
 
-describe('TypeRecipeBuilderClass', () => {
-  it('Should return NoTypeInfo for interfaces', () => {
-    const type = TsTestUtils.getInterface(
-      'ISimple',
-      `
+test('Should return NoTypeInfo for interfaces', () => {
+  const type = TsTestUtils.getInterface(
+    'ISimple',
+    `
 interface ISimple {
 }
 `    );
 
-    const builder = new TypeRecipeBuilderClass();
-    const typeRecipe = builder.create(type, {} as ITypeRecipeContext);
-    expect(typeRecipe).toBeInstanceOf(NoTypeInfo);
-  });
+  const builder = new TypeRecipeBuilderClass();
+  const typeRecipe = builder.create(type, {} as ITypeRecipeContext);
+  expect(typeRecipe).toBeInstanceOf(NoTypeInfo);
+});
 
-  it('Should create type recipe for classes', () => {
-    const type = TsTestUtils.getClass(
-      'TestClass',
-      `
+test('Should create type recipe for classes', () => {
+  const type = TsTestUtils.getClass(
+    'TestClass',
+    `
 class TestClass {
 }
 `    );
 
-    const expectedRequest: ITypeRecipeRequest = {
-      kind: TypeRecipeRequestKind.recipe,
-      value: {
-        className: 'TestClass',
-        fields: []
-      }
-    };
+  const expectedRequest: ITypeRecipeRequest = {
+    kind: TypeRecipeRequestKind.recipe,
+    value: {
+      className: 'TestClass',
+      fields: []
+    }
+  };
 
-    const builder = new TypeRecipeBuilderClass();
-    const typeRecipe = builder.create(type, {} as ITypeRecipeContext);
-    expect(typeRecipe).toEqual(expectedRequest);
-  });
+  const builder = new TypeRecipeBuilderClass();
+  const typeRecipe = builder.create(type, {} as ITypeRecipeContext);
+  expect(typeRecipe).toEqual(expectedRequest);
+});
+
+test('Should create info for explicit constructor', () => {
+  const type = TsTestUtils.getClass(
+    'TestClass',
+    `
+class TestClass {
+  constructor(a: number, b: number) {}
+}
+`    );
+
+  const expectedRequest: ITypeRecipeRequest = {
+    kind: TypeRecipeRequestKind.recipe,
+    value: {
+      fields: [],
+      className: 'TestClass',
+      ctor: {
+        name: 'ctor',
+        arguments: [
+          {name: 'a', typeRecipeRequest: {kind: TypeRecipeRequestKind.number}},
+          {name: 'b', typeRecipeRequest: {kind: TypeRecipeRequestKind.number}},
+        ]
+      }
+    }
+  };
+
+  const nestedRecipeRequest: ITypeRecipeRequest = {kind: TypeRecipeRequestKind.number};
+  const mockContext: Partial<ITypeRecipeContext> = {
+    resolveTypeOrThrow: jest.fn(() => nestedRecipeRequest)
+  };
+
+  const builder = new TypeRecipeBuilderClass();
+  const typeRecipe = builder.create(type, mockContext as ITypeRecipeContext);
+  expect(typeRecipe).toEqual(expectedRequest);
 });
--- packages/core/src/kernel/type-info/type-recipe-builder-class.ts
+++ packages/core/src/kernel/type-info/type-recipe-builder-class.ts
@@ -3,6 +3,7 @@ import {NoTypeInfo} from './no-type-info';
 import {ITypeRecipeBuilder} from './itype-recipe-builder';
 import {ITypeRecipeContext} from './itype-recipe-context';
 import {ITypeRecipeRequest, TypeRecipeRequestKind} from './itype-recipe-request';
+import {TypeTraitBuilderConstructors} from './type-trait-builder-constructors';
 
 /**
  * Can build type recipe for a class.
@@ -20,15 +21,20 @@ export class TypeRecipeBuilderClass implements ITypeRecipeBuilder {
     if(symbol) {
       const isClass = symbol.getDeclarations().some(d => TypeGuards.isClassDeclaration(d));
       if(isClass) {
-        const typeRecipe: ITypeRecipeRequest = {
+        typeRecipeRequest = {
           kind: TypeRecipeRequestKind.recipe,
           value: {
             className: symbol.getFullyQualifiedName(),
             fields: []
           }
         };
-        typeRecipeRequest = typeRecipe;
       }
+    }
+
+    // Explicit constructor info
+    if(typeRecipeRequest && typeRecipeRequest.value) {
+      const constructorBuilder = new TypeTraitBuilderConstructors(context);
+      constructorBuilder.build(type, typeRecipeRequest.value);
     }
 
     return typeRecipeRequest ? typeRecipeRequest : new NoTypeInfo();
